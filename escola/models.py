a1c8a39136cd552e554cbd2e0ed2e1b8aa4d5f53
Antonio Silva <antonio.ffsmail@gmail.com>
2021-09-21 17:43:14 -0300

    created Matricula model e viewset

--- escola/models.py
+++ escola/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=30)
@@ -9,7 +10,6 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 
-
 class Curso(models.Model):
     NIVEL = (
         ('B', 'Básico'),
@@ -19,3 +19,17 @@ class Curso(models.Model):
     codigo_curso = models.CharField(max_length=10)
     descricao = models.CharField(max_length=100)
     nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
+
+
+    def __str__(self):
+        return self.descricao
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Verpertino'),
+        ('N', 'Noturno')
+    )
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False, default='M')
